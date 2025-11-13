d8039d255d24fdb9509b667f9e63fceff8234f8d
pyankovakristina <pyankowa.kristina@gmail.com>
2025-11-13 18:32:16 +0300

    Add files via upload

---
+++ home 1.3/home 1.3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("a*x^2 + b*x + c = 0");
+
+        Console.WriteLine("Введите коэффициент a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите коэффициент b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите коэффициент c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        SolveEquation(a, b, c);
+    }
+
+    static void SolveEquation(double A, double B, double C)
+    {
+        if (A == 0)
+        {
+            if (B == 0)
+            {
+                if (C == 0)
+                {
+                    Console.WriteLine("Все числа являются корнями.");
+                }
+                else
+                {
+                    Console.WriteLine("Нет решений: уравнение является противоречием.");
+                }
+            }
+            else
+            {
+                double x = -C / B;
+                Console.WriteLine($"Линейное уравнение: единственный корень x = {x}");
+            }
+        }
+        else
+        {
+            double d = Math.Pow(B, 2) - 4 * A * C;
+            if (d > 0)
+            {
+                double x1 = (-B + Math.Sqrt(d)) / (2 * A);
+                double x2 = (-B - Math.Sqrt(d)) / (2 * A);
+                Console.WriteLine($"2 корня: x1 = {x1}; x2 = {x2}");
+            }
+            else if (d == 0)
+            {
+                double x = -B / (2 * A);
+                Console.WriteLine($"1 корень: x = {x}");
+            }
+            else
+            {
+                Console.WriteLine("Корней нет.");
+            }
+        }
+    }
+}
---
+++ home 2.2/home 2.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Рекурсивный факториал
+static int FactorialRec(int n)
+{
+    if (n <= 1) return 1;
+    return n * FactorialRec(n - 1);
+}
+
+// Итеративный факториал
+static int FactorialIter(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+// Рекурсивный фибоначчи
+static int FibonacciRec(int n)
+{
+    if (n <= 1) return n;
+    return FibonacciRec(n - 1) + FibonacciRec(n - 2);
+}
+
+// Итеративный фибоначчи
+static int FibonacciIter(int n)
+{
+    if (n <= 1) return n;
+    int a = 0, b = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+Console.WriteLine("Факториал 5 (рекурсивно): " + FactorialRec(5));
+Console.WriteLine("Факториал 5 (итеративно): " + FactorialIter(5));
+Console.WriteLine("Фибоначчи 7 (рекурсивно): " + FibonacciRec(7));
+Console.WriteLine("Фибоначчи 7 (итеративно): " + FibonacciIter(7));
